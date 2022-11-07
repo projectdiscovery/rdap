93d15783fe2912984ddf3f88c6fbe7e7fce13425
mzack <marco.rivoli.nvh@gmail.com>
2022-11-07 19:19:47 +0100

    fixing import paths

--- bootstrap/client.go
+++ bootstrap/client.go
@@ -100,7 +100,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/openrdap/rdap/bootstrap/cache"
+	"github.com/projectdiscovery/rdap/bootstrap/cache"
 )
 
 // A RegistryType represents a bootstrap registry type.
